9407d658b3b8dc4bc3bff1aacdc0ce9212c39e6a
aida wu <ada10050616@gmail.com>
2024-11-22 12:07:22 +0800

    m

--- service/uploadFirebase.js
+++ service/uploadFirebase.js
@@ -3,40 +3,39 @@ const { bucket } = require('./firebase'); // firebase admin
 // 上傳音訊至 Firebase
 const uploadAudioToFirebase = async (buffer, fileName, contentType) => {
     try {
-      console.log('上傳文件至 Firebase:', fileName, '內容類型:', contentType);
-      const blob = bucket.file(fileName); // 建議上傳對象
-      if (!blob) {
-        console.error('無法創建 blob 實例');
-        throw new Error('無法創建 Firebase Storage 文件實例');
-      }
+      const file = bucket.file(fileName);
       console.log('準備將文件上傳到 Firebase:', fileName);
 
-      const blobStream = blob.createWriteStream({
-        metadata: { contentType },
+      // 創建寫入流，將文件數據寫入 Storage
+      const stream = file.createWriteStream({
+        metadata: {
+          contentType: contentType,
+        },
       });
 
       return new Promise((resolve, reject) => {
-        blobStream.on('finish', async () => {
+        stream.on('finish', async () => {
           try {
-            const [url] = await blob.getSignedUrl({
+            // 獲取下載 URL
+            const [url] = await file.getSignedUrl({
               action: 'read',
-              expires: '12-31-2500', // 設定有效期限
+              expires: '12-31-2500',
             });
-            console.log('音訊已成功上傳到 Firebase，公開 URL:', url);
+            console.log('文件已成功上傳到 Firebase，公開 URL:', url);
             resolve(url);
           } catch (error) {
-            console.error('取得公開 URL 失敗:', error.message);
-            reject(new Error('取得公開 URL 失敗'));
+            console.error('獲取公開 URL 失敗:', error.message);
+            reject(new Error('獲取公開 URL 失敗'));
           }
         });
 
-        blobStream.on('error', (error) => {
+        stream.on('error', (error) => {
           console.error('文件上傳過程中失敗:', error.message);
           reject(new Error('文件上傳到 Firebase 失敗'));
         });
 
-        // 寫入 Buffer
-        blobStream.end(buffer);
+        // 將 Buffer 寫入流
+        stream.end(buffer);
       });
     } catch (error) {
         console.error('文件上傳到 Firebase 失敗:', error.message);
